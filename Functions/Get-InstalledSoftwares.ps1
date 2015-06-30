49183a7741ae421725e101df5e92d2de0c685a22
Mooser Lee <mosser@live.cn>
2015-06-30 23:54:46 +0800

    Add function  Get-InstalledSoftwares
    The function return a installed software list by retrieving registry
    from below path;
    1.'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
    2.'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
    3.'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
    Author: Mosser Lee (http://www.pstips.net/author/mosser/)

---
+++ Functions/Get-InstalledSoftwares.ps1
@@ -0,0 +1,47 @@
+﻿<#
+.Synopsis
+   Get installed software list by retrieving registry.
+.DESCRIPTION
+   The function return a installed software list by retrieving registry from below path;
+   1.'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+   2.'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+   3.'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+.EXAMPLE
+   Get-InstalledSoftwares
+.EXAMPLE
+   Get-InstalledSoftwares  | Group-Object Publisher
+#>
+function Get-InstalledSoftwares
+{
+    #
+    # Read registry key as product entity.
+    #
+    function ConvertTo-ProductEntity
+    {
+        param([Microsoft.Win32.RegistryKey]$RegKey)
+        $product = '' | select Name,Publisher,Version
+        $product.Name =  $_.GetValue("DisplayName")
+        $product.Publisher = $_.GetValue("Publisher")
+        $product.Version =  $_.GetValue("DisplayVersion")
+
+        if( -not [string]::IsNullOrEmpty($product.Name)){
+            $product
+        }
+    }
+
+    $UninstallPaths = @(,
+    # For local machine.
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+    # For current user.
+    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall')
+
+    # For 32bit softwares that were installed on 64bit operating system.
+    if([Environment]::Is64BitOperatingSystem) {
+        $UninstallPaths += 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    }
+    $UninstallPaths | foreach {
+        Get-ChildItem $_ | foreach {
+            ConvertTo-ProductEntity -RegKey $_
+        }
+    }
+}
